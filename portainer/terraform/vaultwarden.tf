f186cf7d630e4790250a431f7f7807e0555b1fc2
nmourapt <moura.abt@gmail.com>
2025-06-03 09:00:57 +0100

    update vaultwarden admin token

--- portainer/terraform/vaultwarden.tf
+++ portainer/terraform/vaultwarden.tf
@@ -1,14 +1,3 @@
-variable "vaultwarden_admin_token" {
-  description = "Admin token for Vaultwarden"
-  type        = string
-  sensitive   = true
-}
-
-variable "vaultwarden_domain" {
-  description = "Domain for Vaultwarden"
-  type        = string
-  sensitive   = true
-}
 
 resource "portainer_stack" "vaultwarden_stack" {
   name              = "vaultwarden"
