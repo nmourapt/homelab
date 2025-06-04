a1adb6b1ec2340a8ddfb0326422b35539b307451
nmourapt <moura.abt@gmail.com>
2025-06-04 12:21:53 +0100

    deploy lis_isp

---
+++ cloudflare/terraform/lis_isp.tf
@@ -0,0 +1,63 @@
+
+resource "cloudflare_zero_trust_tunnel_cloudflared" "lis_isp" {
+  account_id = var.cloudflare_account_id
+  name = "lis_isp"
+  config_src = "cloudflare"
+}
+
+output "lis_isp_tunnel_id" {
+  value = cloudflare_zero_trust_tunnel_cloudflared.lis_isp.id
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "lis_isp_config" {
+  depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_isp]
+  account_id = var.cloudflare_account_id
+  tunnel_id = cloudflare_zero_trust_tunnel_cloudflared.lis_isp.id
+  config = {
+    ingress = [
+      {
+        hostname = "omada.${var.tld}"
+        service = "http://192.168.1.99:80"
+        origin_request = {
+          http2_origin = true
+          no_tls_verify = true
+        }
+      },
+      {
+        hostname = "isp.${var.tld}"
+        service = "http://192.168.1.254:80"
+        origin_request = {
+          http2_origin = true
+          no_tls_verify = true
+        }
+      },
+      {
+        service = "http_status:404"
+      }
+    ]
+  }
+}
+
+resource "cloudflare_dns_record" "omada_record" {
+  depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_isp]
+  zone_id = var.cloudflare_tld_zone_id
+  type = "CNAME"
+  name = "omada"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.lis_isp.id}.cfargotunnel.com"
+  ttl = 1
+  comment = "Managed by terraform - do not edit"
+  proxied = true
+  tags = ["terraform", "lis_isp", "cloudflared", "tunnel"]
+}
+
+resource "cloudflare_dns_record" "isp_record" {
+  depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_isp]
+  zone_id = var.cloudflare_tld_zone_id
+  type = "CNAME"
+  name = "isp"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.lis_isp.id}.cfargotunnel.com"
+  ttl = 1
+  comment = "Managed by terraform - do not edit"
+  proxied = true
+  tags = ["terraform", "lis_isp", "cloudflared", "tunnel"]
+}
