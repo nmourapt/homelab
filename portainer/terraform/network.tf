6cee83dd8429f4f76ace59e077fc1a93bc91e80e
nmourapt <moura.abt@gmail.com>
2025-06-04 17:55:06 +0100

    deploy cfd_lis

--- portainer/terraform/network.tf
+++ portainer/terraform/network.tf
@@ -15,6 +15,16 @@ resource "portainer_docker_network" "vlan1config" {
   options = {
     parent = "eth0.1"
   }
+  ipam = {
+    driver = "default"
+    config = [
+      {
+        subnet = "192.168.1.0/24"
+        iprange = "192.168.1.64/27"
+        gateway = "192.168.1.254"
+      }
+    ]
+  }
 }
 
 resource "portainer_docker_network" "vlan1" {
@@ -22,14 +32,4 @@ resource "portainer_docker_network" "vlan1" {
   name        = "vlan1"
   driver      = "macvlan"
   config_from = "vlan1config"
-}
-
-resource "portainer_docker_network" "vlan11config" {
-  endpoint_id = 1
-  name        = "vlan11config"
-  driver      = "macvlan"
-  config_only = true
-  options = {
-    parent = "eth0.11"
-  }
 }
