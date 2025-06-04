568aa9cb4512686f4218884a3fdb3e73921699cf
nmourapt <moura.abt@gmail.com>
2025-06-04 18:09:09 +0100

    deploy cfd_lis

--- portainer/terraform/network.tf
+++ portainer/terraform/network.tf
@@ -24,9 +24,9 @@ resource "portainer_docker_network" "vlan1" {
 }
 
 
-resource "portainer_docker_network" "vlan112config" {
+resource "portainer_docker_network" "vlan11config" {
   endpoint_id = 1
-  name        = "vlan112config"
+  name        = "vlan11config"
   driver      = "macvlan"
   config_only = true
   options = {
