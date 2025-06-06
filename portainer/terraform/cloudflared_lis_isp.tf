725d767bc8b130fe27ef582143c191551eee08c5
nmourapt <moura.abt@gmail.com>
2025-06-06 11:13:31 +0100

    deploy cfd_lis

--- portainer/terraform/cloudflared_lis_isp.tf
+++ portainer/terraform/cloudflared_lis_isp.tf
@@ -4,5 +4,4 @@ resource "portainer_stack" "cloudflared_lis_isp_stack" {
   method            = "file"
   endpoint_id       = 1
   stack_file_path   = "./compose/cloudflared_lis_isp.yml"
-  depends_on        = [portainer_docker_network.vlan1]
 }
