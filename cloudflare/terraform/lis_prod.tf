a544d71dc0535f6984e776c87d307d1af3a51f9d
nmourapt <moura.abt@gmail.com>
2025-06-09 14:59:22 +0100

    deploy qbittorrent

--- cloudflare/terraform/lis_prod.tf
+++ cloudflare/terraform/lis_prod.tf
@@ -24,6 +24,14 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "lis_prod_config" {
         }
       },
       {
+        hostname = "down.${var.tld}"
+        service = "http://qbittorrent:8080"
+        origin_request = {
+          http2_origin = true
+          no_tls_verify = true
+        }
+      },
+      {
         service = "http_status:404"
       }
     ]
@@ -35,6 +43,18 @@ resource "cloudflare_dns_record" "vaultwarden_record" {
   zone_id = var.cloudflare_tld_zone_id
   type = "CNAME"
   name = "warden"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.lis_prod.id}.cfargotunnel.com"
+  ttl = 1
+  comment = "Managed by terraform - do not edit"
+  proxied = true
+  tags = ["terraform", "lis_prod", "cloudflared", "tunnel"]
+}
+
+resource "cloudflare_dns_record" "qbittorrent_record" {
+  depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_prod]
+  zone_id = var.cloudflare_tld_zone_id
+  type = "CNAME"
+  name = "down"
   content = "${cloudflare_zero_trust_tunnel_cloudflared.lis_prod.id}.cfargotunnel.com"
   ttl = 1
   comment = "Managed by terraform - do not edit"
---
+++ portainer/terraform/qbittorrent.tf
@@ -0,0 +1,8 @@
+resource "portainer_stack" "qbittorrent_stack" {
+  name              = "qbittorrent"
+  deployment_type   = "standalone"
+  method            = "file"
+  endpoint_id       = 1
+  stack_file_path   = "./compose/qbittorrent.yml"
+  depends_on        = [portainer_docker_network.prod_network]
+}
