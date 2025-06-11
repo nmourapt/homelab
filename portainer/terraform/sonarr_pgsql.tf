13688d3f1717b0b61b6151a7d2a4f9d75faa1e70
nmourapt <moura.abt@gmail.com>
2025-06-11 16:30:46 +0100

    deploy pgsql for sonarr

---
+++ portainer/terraform/sonarr_pgsql.tf
@@ -0,0 +1,8 @@
+resource "portainer_stack" "sonarr_pgsql_stack" {
+  name              = "sonarr_pgsql"
+  deployment_type   = "standalone"
+  method            = "file"
+  endpoint_id       = 1
+  stack_file_path   = "./compose/sonarr_pgsql.yml"
+  depends_on        = [portainer_docker_network.prod_network]
+}
