d3b8843b28fec6b4750b289ea4817dadc6770b7c
nmourapt <moura.abt@gmail.com>
2025-06-11 16:37:20 +0100

    deploy pgsql for sonarr

--- portainer/terraform/qbittorrent.tf
+++ portainer/terraform/qbittorrent.tf
@@ -3,6 +3,6 @@ resource "portainer_stack" "qbittorrent_stack" {
   deployment_type   = "standalone"
   method            = "file"
   endpoint_id       = 1
-  stack_file_path   = "./compose/qbittorrent.yml"
+  stack_file_path   = "../stacks/qbittorrent/docker-compose.yml"
   depends_on        = [portainer_docker_network.prod_network]
 }
