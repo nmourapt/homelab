054274350d73089507ed77aa640323e568b80c35
nmourapt <moura.abt@gmail.com>
2025-06-16 09:52:03 +0100

    create dns record for HA

--- cloudflare/terraform/lis_prod.tf
+++ cloudflare/terraform/lis_prod.tf
@@ -58,6 +58,18 @@ resource "cloudflare_dns_record" "vaultwarden_record" {
   tags = ["terraform", "lis_prod", "cloudflared", "tunnel"]
 }
 
+resource "cloudflare_dns_record" "homeassistant_record" {
+  depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_prod]
+  zone_id = var.cloudflare_tld_zone_id
+  type = "CNAME"
+  name = "ha"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.lis_prod.id}.cfargotunnel.com"
+  ttl = 1
+  comment = "Managed by terraform - do not edit"
+  proxied = true
+  tags = ["terraform", "lis_prod", "cloudflared", "tunnel"]
+}
+
 resource "cloudflare_dns_record" "qbittorrent_record" {
   depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_prod]
   zone_id = var.cloudflare_tld_zone_id
