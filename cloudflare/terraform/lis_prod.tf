23e9f2bec8c8bc817ab459fc1ef1c9edb694ad3f
nmourapt <moura.abt@gmail.com>
2025-06-16 09:54:02 +0100

    create dns record for HA

--- cloudflare/terraform/lis_prod.tf
+++ cloudflare/terraform/lis_prod.tf
@@ -33,7 +33,7 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "lis_prod_config" {
       },
       {
         hostname = "ha.${var.tld}"
-        service = "http://192.168.1.65:8123"
+        service = "http://192.168.101.65:8123"
         origin_request = {
           http2_origin = true
           no_tls_verify = true
