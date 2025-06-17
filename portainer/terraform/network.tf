d62dab438765eed3a4eebe2092c49a2dc003806e
nmourapt <moura.abt@gmail.com>
2025-06-17 16:12:24 +0100

    remove test network

--- portainer/terraform/network.tf
+++ portainer/terraform/network.tf
@@ -8,6 +8,16 @@ resource "portainer_docker_network" "prod_network" {
   scope       = "local"
 }
 
+resource "portainer_docker_network" "prod_network1" {
+  endpoint_id = 1
+  name        = "prod_network1"
+  driver      = "bridge"
+  internal    = false
+  attachable  = true
+  enable_ipv4 = true
+  scope       = "local"
+}
+
 #resource "portainer_docker_network" "vlan1config" {
 #  endpoint_id = 1
 #  name        = "vlan1config_unused"
