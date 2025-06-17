de266146e79333f9d91da1927e1cbb15666c1725
nmourapt <moura.abt@gmail.com>
2025-06-17 22:45:58 +0100

    try portainer tf version 1.4.4

--- portainer/terraform/network.tf
+++ portainer/terraform/network.tf
@@ -8,30 +8,32 @@ resource "portainer_docker_network" "prod_network" {
   scope       = "local"
 }
 
-#resource "portainer_docker_network" "vlan1config" {
-#  endpoint_id = 1
-#  name        = "vlan1config_unused"
-#  driver      = "macvlan"
-#  config_only = true
-#  options = {
-#    parent = "eth0.1"
-#  }
-#  # on hold - ipam is required but currently not supported on TF provider
-#  #ipam = {
-#  #  driver = "default"
-#  #  config = [
-#  #    {
-#  #      subnet = "192.168.1.0/24"
-#  #      ip_range = "192.168.1.64/27"
-#  #      gateway = "192.168.1.254"
-#  #    }
-#  #  ]
-#  #}
-#}
+resource "portainer_docker_network" "vlan111config" {
+  endpoint_id = 1
+  name        = "vlan111config"
+  driver      = "macvlan"
+  config_only = true
+  enable_ipv4 = true
+  scope       = "local"
+  options = {
+    parent = "eth0.111"
+  }
+  ipam = {
+    driver = "default"
+    config = [
+      {
+        subnet = "192.168.111.0/24"
+        ip_range = "192.168.111.64/27"
+        gateway = "192.168.111.254"
+      }
+    ]
+  }
+}
 
-#resource "portainer_docker_network" "vlan1" {
-#  endpoint_id = 1
-#  name        = "vlan1"
-#  driver      = "macvlan"
-#  config_from = "vlan1config_manual"
-#}
+resource "portainer_docker_network" "vlan111" {
+  endpoint_id = 1
+  name        = "vlan111"
+  driver      = "macvlan"
+  config_from = "vlan111config"
+  depends_on        = [portainer_docker_network.vlan111config]
+}
