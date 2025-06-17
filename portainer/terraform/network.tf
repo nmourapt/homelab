cacd41edf19980d7b5850730090f04616f887bd3
nmourapt <moura.abt@gmail.com>
2025-06-17 23:57:01 +0100

    move cfd_lis_isp network to external

--- portainer/terraform/network.tf
+++ portainer/terraform/network.tf
@@ -15,6 +15,7 @@ resource "portainer_docker_network" "prod_net" {
   }
 }
 
+
 resource "portainer_docker_network" "vlan101config" {
   endpoint_id = 1
   name        = "vlan101config"
@@ -38,4 +39,30 @@ resource "portainer_docker_network" "vlan101" {
   driver      = "macvlan"
   config_from = "vlan101config"
   depends_on  = [portainer_docker_network.vlan101config]
+}
+
+
+resource "portainer_docker_network" "vlan1config" {
+  endpoint_id = 1
+  name        = "vlan1config"
+  driver      = "macvlan"
+  config_only = true
+  enable_ipv4 = true
+  options = {
+    parent = "eth0.1"
+  }
+  ipam_driver = "default"
+  ipam_config {
+    subnet    = "192.168.1.0/24"
+    gateway   = "192.168.1.254"
+    ip_range  = "192.168.1.64/27"
+  }
+}
+
+resource "portainer_docker_network" "vlan1" {
+  endpoint_id = 1
+  name        = "vlan1"
+  driver      = "macvlan"
+  config_from = "vlan1config"
+  depends_on  = [portainer_docker_network.vlan1config]
 }
