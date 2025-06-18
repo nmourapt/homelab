5706401c240f2de334a1ae61c94478659ac70570
nmourapt <moura.abt@gmail.com>
2025-06-18 11:40:19 +0100

    upgrade portainer tf provider to 1.4.5

--- portainer/terraform/network.tf
+++ portainer/terraform/network.tf
@@ -38,6 +38,15 @@ resource "portainer_docker_network" "vlan101" {
   name        = "vlan101"
   driver      = "macvlan"
   config_from = "vlan101config"
+  scope       = "local"
+  options = {
+    parent = "eth0.101"
+  }
+  ipam_config {
+    subnet    = "192.168.101.0/24"
+    gateway   = "192.168.101.254"
+    ip_range  = "192.168.101.64/27"
+  }
   depends_on  = [portainer_docker_network.vlan101config]
 }
 
