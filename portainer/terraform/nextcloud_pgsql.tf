fab55768163e2a4f0696e1301d7179ef857e2d50
nmourapt <moura.abt@gmail.com>
2025-07-14 23:25:52 +0100

    deploy nextcloud pgsql

---
+++ portainer/terraform/nextcloud_pgsql.tf
@@ -0,0 +1,8 @@
+resource "portainer_stack" "nextcloud_pgsql_stack" {
+  name              = "nextcloud_pgsql"
+  deployment_type   = "standalone"
+  method            = "file"
+  endpoint_id       = 1
+  stack_file_path   = "../stacks/nextcloud_pgsql/docker-compose.yml"
+  depends_on        = [portainer_docker_network.prod_net]
+}
