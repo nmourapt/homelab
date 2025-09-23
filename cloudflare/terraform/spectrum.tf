5ebc8f935638efa3b1251de3658808acdbf7075c
nmourapt <moura.abt@gmail.com>
2025-09-23 11:19:48 +0100

    create spectrum domain

---
+++ cloudflare/terraform/spectrum.tf
@@ -0,0 +1,39 @@
+
+resource "cloudflare_zone" "spectrum_zone" {
+  account = {
+    id = var.cloudflare_account_id
+  }
+  name = "spectrum.${var.tld}"
+  type = "full"
+}
+
+resource "cloudflare_zone_subscription" "spectrum_zone_subscription" {
+  zone_id = cloudflare_zone.spectrum_zone.id
+  frequency = "monthly"
+  rate_plan = {
+    id = "enterprise"
+    externally_managed = false
+  }
+}
+
+resource "cloudflare_dns_record" "spectrum_ns1_record" {
+  depends_on = [cloudflare_zone.spectrum_zone]
+  zone_id = var.cloudflare_tld_zone_id
+  type = "NS"
+  name = "spectrum"
+  content = cloudflare_zone.spectrum_zone.name_servers[0]
+  ttl = 1
+  comment = "Managed by terraform - do not edit"
+  tags = ["terraform", "lis_isp", "cloudflared", "tunnel"]
+}
+resource "cloudflare_dns_record" "spectrum_ns2_record" {
+  depends_on = [cloudflare_zone.spectrum_zone]
+  zone_id = var.cloudflare_tld_zone_id
+  type = "NS"
+  name = "spectrum"
+  content = cloudflare_zone.spectrum_zone.name_servers[1]
+  ttl = 1
+  comment = "Managed by terraform - do not edit"
+  tags = ["terraform", "lis_isp", "cloudflared", "tunnel"]
+}
+
