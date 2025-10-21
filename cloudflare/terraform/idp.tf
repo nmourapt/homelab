71c3595f82db45eb79d21cd2ae503b392423ac0f
nmourapt <moura.abt@gmail.com>
2025-10-21 19:33:55 +0100

    deploy pocketid

--- cloudflare/terraform/idp.tf
+++ cloudflare/terraform/idp.tf
@@ -11,7 +11,6 @@ resource "cloudflare_zero_trust_access_identity_provider" "pocketid" {
     certs_url = "https://oidc.${var.tld}/.well-known/jwks.json"
     pkce_enabled = true
     email_claim_name = "email"
-    claims = ["groups"]
-    scopes = ["openid", "email", "profile"]
+    scopes = ["openid", "email", "profile", "groups"]
   }
 }
