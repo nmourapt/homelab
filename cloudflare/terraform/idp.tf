8cafc098c9581f80f0d97cbf0ca0b2ac1416209d
nmourapt <moura.abt@gmail.com>
2025-10-21 18:55:13 +0100

    deploy pocketid

--- cloudflare/terraform/idp.tf
+++ cloudflare/terraform/idp.tf
@@ -4,8 +4,8 @@ resource "cloudflare_zero_trust_access_identity_provider" "pocketid" {
   name       = "Pocket-ID"
   type       = "oidc"
   config = {
-    client_id = ${var.pocketid_client_id}
-    client_secret = ${var.pocketid_client_secret}
+    client_id = var.pocketid_client_id
+    client_secret = var.pocketid_client_secret
     auth_url = "https://oidc.${var.tld}/authorize"
     token_url = "https://oidc.${var.tld}/token"
     certs_url = "https://oidc.${var.tld}/.well-known/openid-configuration"
