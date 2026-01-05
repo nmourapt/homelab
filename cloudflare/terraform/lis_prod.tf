19a72e03568355294efd56fc6d298ec3d87e78cd
nmourapt <moura.abt@gmail.com>
2026-01-05 13:42:14 +0000

    add oauthmail

--- cloudflare/terraform/lis_prod.tf
+++ cloudflare/terraform/lis_prod.tf
@@ -169,3 +169,15 @@ resource "cloudflare_dns_record" "pocketid_record" {
   proxied = true
   tags = ["terraform", "lis_prod", "cloudflared", "tunnel"]
 }
+
+resource "cloudflare_dns_record" "oauthmail_record" {
+  depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_prod]
+  zone_id = var.cloudflare_tld_zone_id
+  type = "CNAME"
+  name = "emailrelay"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.lis_prod.id}.cfargotunnel.com"
+  ttl = 1
+  comment = "Managed by terraform - do not edit"
+  proxied = true
+  tags = ["terraform", "lis_prod", "cloudflared", "tunnel"]
+}
