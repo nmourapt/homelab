249935dba7a56134ad30165620abc48e85718965
nmoura <nmoura@cloudflare.com>
2026-01-14 18:05:48 +0000

    setup dash sso

--- cloudflare/terraform/access_applications.tf
+++ cloudflare/terraform/access_applications.tf
@@ -1,7 +1,11 @@
+# Import with: terraform import cloudflare_zero_trust_access_application.sso_app <account_id>/096f1074-4a80-4dfe-9d9e-0173af52aecc
+# Then run: terraform plan to see the current state and fill in this resource
+
 resource "cloudflare_zero_trust_access_application" "sso_app" {
-  account_id = var.cloudflare_account_id
-  name = "SSO App"
-  type = "dash_sso"
+  account_id       = var.cloudflare_account_id
+  name             = "SSO App"
+  type             = "dash_sso"
+  session_duration = "24h"
 
   allowed_idps = [
     cloudflare_zero_trust_access_identity_provider.pocketid_reauth.id
@@ -12,6 +16,8 @@ resource "cloudflare_zero_trust_access_application" "sso_app" {
     { id = cloudflare_zero_trust_access_policy.pocketid_admins.id },
     { id = cloudflare_zero_trust_access_policy.pocketid_admins_row.id }
   ]
-
-  logo_url = "https://seeklogo.com/images/C/cloudflare-logo-6B7D159387-seeklogo.com.png"
+  
+  saas_app = {
+    auth_type = "saml"
+  }
 }
