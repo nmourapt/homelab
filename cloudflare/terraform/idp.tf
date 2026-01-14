39a54e3a8a3d47d2e9eb4cb9a44bb9e8d4b13e7f
nmoura <nmoura@cloudflare.com>
2026-01-14 14:19:23 +0000

    add reauth new pocket id instance

--- cloudflare/terraform/idp.tf
+++ cloudflare/terraform/idp.tf
@@ -21,3 +21,26 @@ resource "cloudflare_zero_trust_access_identity_provider" "pocketid" {
     user_deprovision = true
   }
 }
+
+resource "cloudflare_zero_trust_access_identity_provider" "pocketid_reauth" {
+  account_id = var.cloudflare_account_id
+  name       = "Pocket ID Reauth"
+  type       = "oidc"
+  config = {
+    client_id = var.pocketid_reauth_client_id
+    client_secret = var.pocketid_client_secret
+    auth_url = "https://oidc.${var.tld}/authorize"
+    token_url = "https://oidc.${var.tld}/api/oidc/token"
+    certs_url = "https://oidc.${var.tld}/.well-known/jwks.json"
+    pkce_enabled = true
+    email_claim_name = "email"
+    claims = ["groups"]
+    scopes = ["openid", "email", "profile", "groups"]
+  }
+  scim_config = {
+    enabled = true
+    identity_update_behavior = "automatic"
+    seat_deprovision = true
+    user_deprovision = true
+  }
+}
