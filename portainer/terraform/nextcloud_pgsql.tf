ad77bac1203264d18e0ef79aaf73c01dd887c473
nmoura <nmoura@cloudflare.com>
2026-01-15 16:51:40 +0000

    access for qbit

--- portainer/terraform/nextcloud_pgsql.tf
+++
@@ -1,8 +0,0 @@
-resource "portainer_stack" "nextcloud_pgsql_stack" {
-  name              = "nextcloud_pgsql"
-  deployment_type   = "standalone"
-  method            = "file"
-  endpoint_id       = 1
-  stack_file_path   = "../stacks/nextcloud_pgsql/docker-compose.yml"
-  depends_on        = [portainer_docker_network.vlan101]
-}
