5e52837a26daf92fb195d1f96080a7532894ecb7
nmoura <nmoura@cloudflare.com>
2026-01-15 15:35:40 +0000

    enable vaultwarden sso

--- cloudflare/terraform/access_applications.tf
+++ cloudflare/terraform/access_applications.tf
@@ -84,3 +84,36 @@ resource "cloudflare_zero_trust_access_application" "vaultwarden_app" {
     }
   }
 }
+
+resource "cloudflare_zero_trust_access_application" "vaultwarden_admin" {
+  account_id           = var.cloudflare_account_id
+  name                 = "TF - Vaultwarden Admin"
+  type                 = "self_hosted"
+  session_duration     = "30m"
+
+  allowed_idps = [
+    cloudflare_zero_trust_access_identity_provider.pocketid_reauth.id
+  ]
+  auto_redirect_to_identity = true
+
+  policies = [
+    { 
+      id = cloudflare_zero_trust_access_policy.pocketid_admins.id,
+      precedence = 1
+    },
+    { 
+      id = cloudflare_zero_trust_access_policy.pocketid_admins_row.id,
+      precedence = 2
+    }
+  ]
+
+  app_launcher_visible = true
+  logo_url = "https://s3-eu-west-1.amazonaws.com/tpd/logos/5c92a3d0634b4d00012fa165/0x0.png"
+
+  destinations = [
+    { 
+      type = "public"
+      uri = "warden.${var.tld}/admin"
+    }
+  ]
+}
