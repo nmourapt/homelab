63ebdb303faa8dadb9fd97701ba405242ea1b60d
nmoura <nmoura@cloudflare.com>
2026-02-02 10:42:25 +0000

    deploy mailcow

--- portainer/terraform/mailcow.tf
+++ portainer/terraform/mailcow.tf
@@ -1,8 +1,8 @@
-resource "portainer_stack" "oauthmail_stack" {
-  name              = "oauthmail"
+resource "portainer_stack" "mailcow_stack" {
+  name              = "mailcow"
   deployment_type   = "standalone"
   method            = "file"
   endpoint_id       = 1
-  stack_file_path   = "../stacks/oauthmail/docker-compose.yml"
+  stack_file_path   = "../stacks/mailcow/docker-compose.yml"
   depends_on        = [portainer_docker_network.prod_net]
 }
