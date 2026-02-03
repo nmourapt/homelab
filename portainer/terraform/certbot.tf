b1050eb74172a647611e788873058f6877734cc6
nmoura <nmoura@cloudflare.com>
2026-02-03 11:32:14 +0000

    deploy omni

---
+++ portainer/terraform/certbot.tf
@@ -0,0 +1,7 @@
+resource "portainer_stack" "certbot_stack" {
+  name              = "certbot"
+  deployment_type   = "standalone"
+  method            = "file"
+  endpoint_id       = 1
+  stack_file_path   = "../stacks/certbot/docker-compose.yml"
+}
