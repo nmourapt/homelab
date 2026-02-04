3f2bcdb28cf613a4d7a4bece84aa3058dfc4b3d9
nmoura <nmoura@cloudflare.com>
2026-02-04 11:37:39 +0000

    move omni behind cf

--- cloudflare/terraform/dns.tf
+++ cloudflare/terraform/dns.tf
@@ -1,4 +1,16 @@
 
+resource "cloudflare_dns_record" "omniapi_record" {
+  depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_prod]
+  zone_id = var.cloudflare_tld_zone_id
+  type = "CNAME"
+  name = "clusterapi"
+  content = "home.${var.tld}"
+  ttl = 1
+  comment = "Managed by terraform - do not edit"
+  proxied = false
+  tags = ["terraform", "lis_prod", "cloudflared", "tunnel"]
+}
+
 resource "cloudflare_dns_record" "omni_record" {
   depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_prod]
   zone_id = var.cloudflare_tld_zone_id
@@ -7,6 +19,6 @@ resource "cloudflare_dns_record" "omni_record" {
   content = "home.${var.tld}"
   ttl = 1
   comment = "Managed by terraform - do not edit"
-  proxied = false
+  proxied = true
   tags = ["terraform", "lis_prod", "cloudflared", "tunnel"]
 }
