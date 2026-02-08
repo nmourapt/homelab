b4ad1f3a7485ed90239a02f50c8c6546538065b2
nmoura <nmoura@cloudflare.com>
2026-02-08 21:58:11 +0000

    move jetkvm to TF

--- cloudflare/terraform/access_applications.tf
+++ cloudflare/terraform/access_applications.tf
@@ -217,6 +217,38 @@ resource "cloudflare_zero_trust_access_application" "netboot" {
   ]
 }
 
+resource "cloudflare_zero_trust_access_application" "jetkvm" {
+  account_id           = var.cloudflare_account_id
+  name                 = "TF - JetKVM"
+  type                 = "self_hosted"
+  session_duration     = "24h"
+
+  allowed_idps = [
+    cloudflare_zero_trust_access_identity_provider.pocketid.id
+  ]
+  auto_redirect_to_identity = true
+
+  policies = [
+    { 
+      id = cloudflare_zero_trust_access_policy.pocketid_admins.id,
+      precedence = 1
+    },
+    { 
+      id = cloudflare_zero_trust_access_policy.pocketid_admins_row.id,
+      precedence = 2
+    }
+  ]
+
+  app_launcher_visible = true
+  logo_url = "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/jetkvm.png"
+
+  destinations = [
+    { 
+      type = "public"
+      uri = "kvm.${var.tld}"
+    }
+  ]
+}
 
 resource "cloudflare_zero_trust_access_application" "omni_app" {
   account_id           = var.cloudflare_account_id
