4cef0d9ee035e9ee305d89cf0fd0a8fcd5afde1b
nmoura <nmoura@cloudflare.com>
2026-02-10 23:14:13 +0000

    expose traefik dashboard

--- cloudflare/terraform/access_applications.tf
+++ cloudflare/terraform/access_applications.tf
@@ -346,3 +346,36 @@ resource "cloudflare_zero_trust_access_application" "argo_oidc" {
     ]
   }
 }
+
+resource "cloudflare_zero_trust_access_application" "traefik" {
+  account_id           = var.cloudflare_account_id
+  name                 = "TF - Traefik"
+  type                 = "self_hosted"
+  session_duration     = "24h"
+
+  allowed_idps = [
+    cloudflare_zero_trust_access_identity_provider.pocketid.id
+  ]
+  auto_redirect_to_identity = true
+
+  policies = [
+    { 
+      id = cloudflare_zero_trust_access_policy.pocketid_admins.id,
+      precedence = 1
+    },
+    { 
+      id = cloudflare_zero_trust_access_policy.pocketid_admins_row.id,
+      precedence = 2
+    }
+  ]
+
+  app_launcher_visible = true
+  logo_url = "https://www.onworks.net/imagescropped/traefikicon.png_3.webp"
+
+  destinations = [
+    { 
+      type = "public"
+      uri = "traefik.${var.tld}"
+    }
+  ]
+}
