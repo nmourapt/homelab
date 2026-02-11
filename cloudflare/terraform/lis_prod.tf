4c8e1e355936f63e1f1056b2cff9bff1773d5a20
nmoura <nmoura@cloudflare.com>
2026-02-11 12:17:32 +0000

    move netboot behind traefik

--- cloudflare/terraform/lis_prod.tf
+++ cloudflare/terraform/lis_prod.tf
@@ -205,16 +205,3 @@ resource "cloudflare_dns_record" "uploadassistant_record" {
   proxied = true
   tags = ["terraform", "lis_prod", "cloudflared", "tunnel"]
 }
-
-resource "cloudflare_dns_record" "netboot_record" {
-  depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_prod]
-  zone_id = var.cloudflare_tld_zone_id
-  type = "CNAME"
-  name = "netboot"
-  content = "${cloudflare_zero_trust_tunnel_cloudflared.lis_prod.id}.cfargotunnel.com"
-  ttl = 1
-  comment = "Managed by terraform - do not edit"
-  proxied = true
-  tags = ["terraform", "lis_prod", "cloudflared", "tunnel"]
-}
-
