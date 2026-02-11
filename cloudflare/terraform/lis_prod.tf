2c4de467fffcfc97718304c6c4e7949289156870
nmoura <nmoura@cloudflare.com>
2026-02-11 14:37:38 +0000

    move immich behind traefik

--- cloudflare/terraform/lis_prod.tf
+++ cloudflare/terraform/lis_prod.tf
@@ -20,14 +20,6 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "lis_prod_config" {
         }
       },
       {
-        hostname = "photos.${var.tld}"
-        service = "http://immich-server:2283"
-        origin_request = {
-          http2_origin = true
-          no_tls_verify = true
-        }
-      },
-      {
         hostname = "media.${var.tld}"
         service = "http://plex:32400"
         origin_request = {
@@ -107,18 +99,6 @@ resource "cloudflare_dns_record" "homeassistant_record" {
   zone_id = var.cloudflare_tld_zone_id
   type = "CNAME"
   name = "ha"
-  content = "${cloudflare_zero_trust_tunnel_cloudflared.lis_prod.id}.cfargotunnel.com"
-  ttl = 1
-  comment = "Managed by terraform - do not edit"
-  proxied = true
-  tags = ["terraform", "lis_prod", "cloudflared", "tunnel"]
-}
-
-resource "cloudflare_dns_record" "immich_record" {
-  depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_prod]
-  zone_id = var.cloudflare_tld_zone_id
-  type = "CNAME"
-  name = "photos"
   content = "${cloudflare_zero_trust_tunnel_cloudflared.lis_prod.id}.cfargotunnel.com"
   ttl = 1
   comment = "Managed by terraform - do not edit"
