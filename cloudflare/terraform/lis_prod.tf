68907cbe7990ec3fb52953a98df7f2830b4f2665
nmoura <nmoura@cloudflare.com>
2026-02-11 14:43:02 +0000

    move plex behind traefik

--- cloudflare/terraform/lis_prod.tf
+++ cloudflare/terraform/lis_prod.tf
@@ -20,14 +20,6 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "lis_prod_config" {
         }
       },
       {
-        hostname = "media.${var.tld}"
-        service = "http://plex:32400"
-        origin_request = {
-          http2_origin = true
-          no_tls_verify = true
-        }
-      },
-      {
         hostname = "oidc.${var.tld}"
         service = "http://pocketid:1411"
         origin_request = {
@@ -99,18 +91,6 @@ resource "cloudflare_dns_record" "homeassistant_record" {
   zone_id = var.cloudflare_tld_zone_id
   type = "CNAME"
   name = "ha"
-  content = "${cloudflare_zero_trust_tunnel_cloudflared.lis_prod.id}.cfargotunnel.com"
-  ttl = 1
-  comment = "Managed by terraform - do not edit"
-  proxied = true
-  tags = ["terraform", "lis_prod", "cloudflared", "tunnel"]
-}
-
-resource "cloudflare_dns_record" "plex_record" {
-  depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_prod]
-  zone_id = var.cloudflare_tld_zone_id
-  type = "CNAME"
-  name = "media"
   content = "${cloudflare_zero_trust_tunnel_cloudflared.lis_prod.id}.cfargotunnel.com"
   ttl = 1
   comment = "Managed by terraform - do not edit"
