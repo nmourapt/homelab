cc2d368dcc20cc5ffef4735ed51533b2a8cc88e5
nmourapt <moura.abt@gmail.com>
2026-02-23 10:58:38 +0000

    deploy bazarr: subtitle manager (subs.nmoura.pt, CF Access admin-only, NFS media)

--- cloudflare/terraform/access_applications.tf
+++ cloudflare/terraform/access_applications.tf
@@ -761,3 +761,36 @@ resource "cloudflare_zero_trust_access_application" "profilarr" {
     }
   ]
 }
+
+resource "cloudflare_zero_trust_access_application" "bazarr" {
+  account_id           = var.cloudflare_account_id
+  name                 = "TF - Bazarr"
+  type                 = "self_hosted"
+  session_duration     = "24h"
+
+  allowed_idps = [
+    cloudflare_zero_trust_access_identity_provider.pocketid.id
+  ]
+  auto_redirect_to_identity = true
+
+  policies = [
+    { 
+      id = cloudflare_zero_trust_access_policy.pocketid_admins.id,
+      precedence = 1
+    },
+    { 
+      id = cloudflare_zero_trust_access_policy.pocketid_admins_row.id,
+      precedence = 2
+    }
+  ]
+
+  app_launcher_visible = true
+  logo_url = "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/bazarr.png"
+
+  destinations = [
+    { 
+      type = "public"
+      uri = "subs.${var.tld}"
+    }
+  ]
+}
