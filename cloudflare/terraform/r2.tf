848cd75b41385e918b0cca1ce9868d048c399722
nmourapt <moura.abt@gmail.com>
2026-03-12 12:46:40 +0000

    feat: add R2 bucket for Longhorn volume backups

--- cloudflare/terraform/r2.tf
+++ cloudflare/terraform/r2.tf
@@ -3,3 +3,9 @@ resource "cloudflare_r2_bucket" "omni_backups" {
   name       = "omni-backups"
   location   = "WEUR"
 }
+
+resource "cloudflare_r2_bucket" "longhorn_backups" {
+  account_id = var.cloudflare_account_id
+  name       = "longhorn-backups"
+  location   = "WEUR"
+}
