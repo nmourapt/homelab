2f7fc00c7ea4f1448505b8cbc968a631b8c5cee1
nmourapt <moura.abt@gmail.com>
2026-03-24 16:50:41 +0000

    create openclaw service token

--- cloudflare/terraform/service_auth.tf
+++ cloudflare/terraform/service_auth.tf
@@ -4,3 +4,9 @@ resource "cloudflare_zero_trust_access_service_token" "prometheus_cf_metrics" {
   name       = "TF - Prometheus cf-metrics"
   duration   = "forever"
 }
+
+resource "cloudflare_zero_trust_access_service_token" "openclaw_service_token" {
+  account_id = var.cloudflare_account_id
+  name       = "TF - Openclaw WARP"
+  duration   = "forever"
+}
