2324fe9e4bce7bc651b2b6400ca2e0232fc68ac3
nmourapt <moura.abt@gmail.com>
2026-04-07 02:45:17 +0100

    change vlan1 addressing

--- portainer/terraform/network.tf
+++ portainer/terraform/network.tf
@@ -51,3 +51,38 @@ resource "portainer_docker_network" "vlan101" {
   depends_on  = [portainer_docker_network.vlan101config]
 }
 
+
+resource "portainer_docker_network" "vlan1config" {
+  endpoint_id = 1
+  name        = "vlan1config"
+  driver      = "macvlan"
+  config_only = true
+  enable_ipv4 = true
+  scope       = "local"
+  options = {
+    parent = "eth0.1"
+  }
+  ipam_driver = "default"
+  ipam_config {
+    subnet    = "192.168.111.0/24"
+    gateway   = "192.168.111.254"
+    ip_range  = "192.168.111.64/27"
+  }
+}
+
+resource "portainer_docker_network" "vlan1" {
+  endpoint_id = 1
+  name        = "vlan1"
+  driver      = "macvlan"
+  config_from = "vlan1config"
+  scope       = "local"
+  options = {
+    parent = "eth0.1"
+  }
+  ipam_config {
+    subnet    = "192.168.111.0/24"
+    gateway   = "192.168.111.254"
+    ip_range  = "192.168.111.64/27"
+  }
+  depends_on  = [portainer_docker_network.vlan1config]
+}
