008083784b77af7d445007748ef557f4682933bd
nmourapt <moura.abt@gmail.com>
2026-06-08 10:33:12 +0100

    deploy opencode oauth

--- cloudflare/terraform/lis_opencode.tf
+++ cloudflare/terraform/lis_opencode.tf
@@ -20,6 +20,10 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "lis_opencode_config"
         }
       },
       {
+        hostname = "opencodeoauth.${var.tld}"
+        service  = "http://host.docker.internal:19876"
+      },
+      {
         hostname = "opencodessh.${var.tld}"
         service  = "ssh://host.docker.internal:22"
       },
@@ -47,6 +51,18 @@ resource "cloudflare_dns_record" "opencodessh_record" {
   zone_id    = var.cloudflare_tld_zone_id
   type       = "CNAME"
   name       = "opencodessh"
+  content    = "${cloudflare_zero_trust_tunnel_cloudflared.lis_opencode.id}.cfargotunnel.com"
+  ttl        = 1
+  comment    = "Managed by terraform - do not edit"
+  proxied    = true
+  tags       = ["terraform", "lis_opencode", "cloudflared", "tunnel"]
+}
+
+resource "cloudflare_dns_record" "opencodeoauth_record" {
+  depends_on = [cloudflare_zero_trust_tunnel_cloudflared.lis_opencode]
+  zone_id    = var.cloudflare_tld_zone_id
+  type       = "CNAME"
+  name       = "opencodeoauth"
   content    = "${cloudflare_zero_trust_tunnel_cloudflared.lis_opencode.id}.cfargotunnel.com"
   ttl        = 1
   comment    = "Managed by terraform - do not edit"
