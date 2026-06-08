fad98537204510f3f00d57a7fbebee0ebaaab1d4
nmourapt <moura.abt@gmail.com>
2026-06-08 11:17:16 +0100

    create home IP list

--- cloudflare/terraform/zt_lists.tf
+++ cloudflare/terraform/zt_lists.tf
@@ -6,6 +6,13 @@ resource "cloudflare_zero_trust_list" "egress_ips" {
   items       = jsondecode(var.egress_ips_json)
 }
 
+resource "cloudflare_zero_trust_list" "home_ip" {
+  account_id  = var.cloudflare_account_id
+  name        = "TF - Home IP"
+  type        = "IP"
+  description = "Home IP"
+}
+
 resource "cloudflare_zero_trust_list" "personal_emails" {
   account_id  = var.cloudflare_account_id
   name        = "TF - Personal Emails"
